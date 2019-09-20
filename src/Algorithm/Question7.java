021c86b7afcaf34768e528230ee3df7c8770bd0d
Carmen-kaman <cahejiamin@163.com>
2019-09-20 14:47:20 -0400

    Question 7 add answer
    Add the answer of Question 7

--- src/Algorithm/Question7.java
+++ src/Algorithm/Question7.java
@@ -42,4 +42,18 @@ public class Question7 {
         }
     }
 
+    public int solution(int x){
+        int num = 0;
+        while(x != 0){
+            int pop = x % 10;
+            x /= 10;
+            if (num > Integer.MAX_VALUE/10 ||
+                    (num == Integer.MAX_VALUE/10 && pop >8))return 0;
+            if (num < Integer.MIN_VALUE/10 ||
+                    (num == Integer.MIN_VALUE/10 && pop<-8)) return 0;
+            num = num *10 +pop;
+        }
+        return num;
+    }
+
 }
