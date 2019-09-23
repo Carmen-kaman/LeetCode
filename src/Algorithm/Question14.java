222d1977bc25d542bb5586e6936e321cb5099892
Carmen-kaman <cahejiamin@163.com>
2019-09-23 18:20:28 -0400

    add solution Question14

--- src/Algorithm/Question14.java
+++ src/Algorithm/Question14.java
@@ -12,8 +12,8 @@ import java.util.Comparator;
  */
 public class Question14 {
     public static void main(String[] args) {
-        String[] strings = {"dog","racecar","car"};
-        System.out.println(new Question14().longestCommonPrefix(strings));
+        String[] strings = {"flower","flow"};
+        System.out.println(new Question14().solution(strings));
     }
 
     public String longestCommonPrefix(String[] strs) {
@@ -41,6 +41,20 @@ public class Question14 {
                     }
                 }
                 prefix += String.valueOf(curr);
+            }
+        }
+        return prefix;
+    }
+
+    public String solution (String[] strs){
+        if (strs.length == 0){
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) return "";
             }
         }
         return prefix;
